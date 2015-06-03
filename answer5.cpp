1e1eff695f511edcca6e62e346ad0b64fc8cbde5
EKPENYONG-EMMANUEL <emmanuelekpenyong28@gmail.com>
2015-06-03 12:37:50 +0100

    Update answer5.cpp

--- answer5.cpp
+++ answer5.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+int pow();
 int main()
 {
 	cout << "integer\t\tsquare\t\tcube\n";
-	cout << 0 << "\t\t" << 0*0 << "\t\t" << 0*0*0 <<"\n";
-	cout << 1 << "\t\t" << 1*1 << "\t\t" << 1*1*1 <<"\n";
-	cout << 2 << "\t\t" << 2*2 << "\t\t" << 2*2*2 <<"\n";
-	cout << 3 << "\t\t" << 3*3 << "\t\t" << 3*3*3 <<"\n";
-	cout << 4 << "\t\t" << 4*4 << "\t\t" << 4*4*4 <<"\n";
-	cout << 5 << "\t\t" << 5*5 << "\t\t" << 5*5*5 <<"\n";
-	cout << 6 << "\t\t" << 6*6 << "\t\t" << 6*6*6 <<"\n";
-	cout << 7 << "\t\t" << 7*7 << "\t\t" << 7*7*7 <<"\n";
-	cout << 8 << "\t\t" << 8*8 << "\t\t" << 8*8*8 <<"\n";
-	cout << 9 << "\t\t" << 9*9 << "\t\t" << 9*9*9 <<"\n";
-	cout << 10 << "\t\t" << 10*10 << "\t\t" << 10*10*10 <<"\n";
+	cout << 0 << "\t\t" << pow(0,2) << "\t\t" << pow(0,3) <<"\n";
+	cout << 1 << "\t\t" << pow(1,2) << "\t\t" << pow(1,3) <<"\n";
+	cout << 2 << "\t\t" << pow(2,2) << "\t\t" << pow(2,3) <<"\n";
+	cout << 3 << "\t\t" << pow(3,2) << "\t\t" << pow(3,3) <<"\n";
+	cout << 4 << "\t\t" << pow(4,2) << "\t\t" << pow(4,3) <<"\n";
+	cout << 5 << "\t\t" << pow(5,2) << "\t\t" << pow(5,3) <<"\n";
+	cout << 6 << "\t\t" << pow(6,2) << "\t\t" << pow(6,3) <<"\n";
+	cout << 7 << "\t\t" << pow(7,2) << "\t\t" << pow(7,3) <<"\n";
+	cout << 8 << "\t\t" << pow(8,2) << "\t\t" << pow(8,3) <<"\n";
+	cout << 9 << "\t\t" << pow(9,2) << "\t\t" << pow(9,3) <<"\n";
+	cout << 10 << "\t\t" << pow(10,2) << "\t\t" << pow(10,3) <<"\n";
 	
 	return 0;
 }
